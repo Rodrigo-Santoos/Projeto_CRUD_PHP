4c82250e2591219d89b3b600c7f608c6dd117a29
rodrigo-santoos <rodrigoyukimide@gmail.com>
2024-09-21 17:18:36 -0300

    adicionando config de conexao com o banco

--- index.php
+++ index.php
@@ -4,10 +4,47 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastro</title>
-    <link href="css/bootstrap-grid.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Cadastro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="?page=novo">Novo Usuario</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=listar">Listar Usuario</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+            <?php
+                include("config.php");
+                switch(@$_REQUEST["page"]){
+                    case "novo":
+                        include("novo-usuario.php");
+                        break;
+                    case "listar":
+                        include ("listando-usuario.php");
+                        break;
+                    default:
+                    print "<h1>bem vindos</>";    
+                }
+             ?>
+            </div>
+        </div>
+    </div>
     <script src="js/bootstrap.bundle.min.js" ></script>
   </body>
 </html>

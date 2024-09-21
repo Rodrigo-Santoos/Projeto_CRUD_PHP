ddcda4dc5714ebdf8a8a9460112c7a6c546a0aff
rodrigo-santoos <rodrigoyukimide@gmail.com>
2024-09-21 16:06:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro</title>
+    <link href="css/bootstrap-grid.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="js/bootstrap.bundle.min.js" ></script>
+  </body>
+</html>
